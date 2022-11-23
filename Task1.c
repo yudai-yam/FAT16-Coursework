b9900094338c1cdbee4e85ec8571440051fbbd4f
Yudai <yudaimadeinjapan@gmail.com>
2022-11-23 22:37:47 +0000

    done with task 1

--- Task1.c
+++ Task1.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+
+void fileReader(char* file, off_t offset, int readingByte){
+    char arr[100];
+    int fileDescriptor = open(file, O_RDONLY);
+
+    lseek(fileDescriptor, offset, SEEK_CUR);
+    read(fileDescriptor, arr, readingByte);
+    printf("The result (offset = %i, readingByte = %i) is \n%s", offset, readingByte, arr);
+    close(fileDescriptor);
+}
+
+int main(){
+    fileReader("Task1.txt", 5, 10);
+    return 0;
+}
